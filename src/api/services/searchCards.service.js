149f03020963f4ab30948ccb3a069a3124e1211a
ozzzpo <onehendrix04@gmail.com>
2024-02-25 00:00:21 +0300

    start implementing search searchCards

---
+++ src/api/services/searchCards.service.js
@@ -0,0 +1,21 @@
+import apiClient from "../instance";
+const searchCardsApi = () => {
+  return {
+    getSearchCards: () => {
+      return apiClient.get("search_cards/my");
+    },
+    getSearchCardById: (cardId) => {
+      return apiClient.get(`search_cards/${cardId}`);
+    },
+    addSearchCard: (newCard) => {
+      return apiClient.post("search_cards", newCard);
+    },
+    updateSearchCard: (cardId, updatedCard) => {
+      return apiClient.patch(`search_cards/${cardId}`, updatedCard);
+    },
+    getFirstActiveSearchCards: () => {
+      return apiClient.get("search_cards");
+    },
+  };
+};
+export default searchCardsApi;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
 import petsSlice from "./pets/petsSlice";
+import searchCardsSlice from "./searchCards/searchCardsSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     pets: petsSlice,
+    searchCards: searchCardsSlice,
   },
 });
