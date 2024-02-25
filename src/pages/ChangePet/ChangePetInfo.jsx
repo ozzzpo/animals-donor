b89b1c7e7e064e27760c58be0ca2c43751dee7a9
Warnoffline <hametshin543@gmail.com>
2024-02-25 04:46:55 +0300

    forms

--- src/pages/ChangePet/ChangePetInfo.jsx
+++ src/pages/ChangePet/ChangePetInfo.jsx
@@ -3,31 +3,34 @@ import React from "react";
 function ChangePetInfo({ pet }) {
   return (
     <div className='change-pet__info'>
-      <h2>{pet?.pet_type?.name}</h2>
-      <div className='change-pet__block'>
-        <p>Порода: </p>
-        <label>{pet?.breed}</label>
-      </div>
-      <div className='change-pet__block'>
-        <p>Группа крови: </p>
-        <label>{pet?.blood_type}</label>
-      </div>
+      <h2>{pet?.name}</h2>
       <div className='change-pet__block'>
         <p>Дата рождения: </p>
         <label>{pet?.birthday}</label>
       </div>
-      <div className='change-pet__block'>
-        <p>Вес: </p>
-        <label>{pet?.weight}</label>
+      <div className="change-pet__blocks">
+        <div className='change-pet__block'>
+          <p>Порода: </p>
+          <label>{pet?.breed}</label>
+        </div>
+        <div className='change-pet__block'>
+          <p>Группа крови: </p>
+          <label>{pet?.blood_type}</label>
+        </div>
+        <div className='change-pet__block'>
+          <p>Вес: </p>
+          <label>{pet?.weight}</label>
+        </div>
+        <div className='change-pet__block'>
+          <p>Не доступен:</p>
+          <label>{pet?.unavailable_lists}</label>
+        </div>
+        <div className='change-pet__block'>
+          <p>Вакцины:</p>
+          <label>{pet?.vaccinations}</label>
+        </div>
       </div>
-      <div className='change-pet__block'>
-        <p>Не доступен:</p>
-        <label>{pet?.unavailable_lists}</label>
-      </div>
-      <div className='change-pet__block'>
-        <p>Вакцины:</p>
-        <label>{pet?.vaccinations}</label>
-      </div>
+      
     </div>
   );
 }
